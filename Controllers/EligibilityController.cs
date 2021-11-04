b3c0f2dacb3d4a0a9ec96fc7963b65054ed5aeaf
Osama Aleem <osamaaleem32@outlook.com>
2021-11-04 11:46:31 +0500

    Add project files.

---
+++ Controllers/EligibilityController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using WE_Asgn_2.Models;
+
+namespace WE_Asgn_2.Controllers
+{
+    public class EligibilityController : Controller
+    {
+        // GET: Eligibility
+        ElgCalculator elgCalculator1 = new ElgCalculator();
+        public ActionResult Index()
+        {
+            ViewBag.Provinces = elgCalculator1.ProvinceList();
+            ViewBag.Eligible = "-------";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(ElgCalculator elgCalculator)
+        {
+            ViewBag.Provinces = elgCalculator.ProvinceList();
+            ViewBag.Eligible = elgCalculator.IsEligible();
+            return View();
+        }
+    }
+}
---
+++ Models/ElgCalculator.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace WE_Asgn_2.Models
+{
+    public class ElgCalculator
+    {
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public char Gender { get; set; }
+        public string Qualification { get; set; }
+        public int ObtainedMarks { get; set; }
+        public int TotalMarks { get; set; }
+        public string Province { get; set; }
+        public List<SelectListItem> ProvinceList()
+        {
+            List<SelectListItem> provinceList = new List<SelectListItem>();
+            provinceList.Add(new SelectListItem { Text = "Punjab", Value = "Punjab" });
+            provinceList.Add(new SelectListItem { Text = "Sindh", Value = "Sindh" });
+            provinceList.Add(new SelectListItem { Text = "KPK", Value = "KPK" });
+            provinceList.Add(new SelectListItem { Text = "Baluchistan", Value = "Baluchistan" });
+            return provinceList;
+        }
+        private int Percentage()
+        {
+            int percentage;
+            percentage = (int)Math.Round((double)(ObtainedMarks * 100) / TotalMarks);
+            return percentage;
+        }
+        public string IsEligible()
+        {
+            string province = Province.ToLower();
+            switch (Qualification.ToUpper())
+            {
+                case "FSC":
+                    if (province == "punjab" && Percentage() >= 70)
+                    {
+                        return "Yes";
+                    }
+                    else if (province != "punjab" && Percentage() >= 60)
+                    {
+                        return "Yes";
+                    }
+                    else
+                    {
+                        return "No";
+                    }
+                case "BCS":
+                    if (province == "punjab" && Percentage() >= 60)
+                    {
+                        return "Yes";
+                    }
+                    else if (province != "punjab" && Percentage() >= 50)
+                    {
+                        return "Yes";
+                    }
+                    else
+                    {
+                        return "No";
+                    }
+                default:
+                    return "Wrong Entry";
+            }
+        }
+    }
+}
